c4ec44277bb7cb4dda57c852848e506b014f8233
Phil <philip.falkenberg@accenture.com>
2017-10-25 12:16:42 -0400

    add todo text and button

--- src/view/todo.js
+++ src/view/todo.js
@@ -1,13 +1,34 @@
 import React from "react";
-import "../css/todo.scss";
+import { Button, FormControl, Col, Row } from 'react-bootstrap';
 
 // app component
 export default class Todo extends React.Component {
   // render
+
+  onButtonClick() {
+    console.log('test click')
+  }
+
   render() {
     return (
       <div className="container">
-        Todo app
+        <br/>
+
+        <Row>
+        <Col xs={3}>
+            <FormControl
+                type="text"
+                placeholder="Enter text"
+                bsSize="sm"
+            />
+        </Col>
+
+        <Col xs={9}>
+            <Button bsStyle="default" bsSize="small" onClick={this.onButtonClick}>
+                Add
+            </Button>
+        </Col>
+        </Row>
       </div>
     );
   }
