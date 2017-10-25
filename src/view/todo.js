60900308adaf189b17feafe35067d9ed66cb90d4
Phil <philip.falkenberg@accenture.com>
2017-10-25 11:11:34 -0400

    start todo app

---
+++ src/view/todo.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../css/todo.scss";
+
+// app component
+export default class Todo extends React.Component {
+  // render
+  render() {
+    return (
+      <div className="container">
+        Todo app
+      </div>
+    );
+  }
+}
